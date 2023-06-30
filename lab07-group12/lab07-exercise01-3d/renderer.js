bddd30a15c76414d514c13035eb94d7347f6209d
sara-mc13 <117697158+sara-mc13@users.noreply.github.com>
2023-06-30 12:44:40 -0500

    Add files via upload

---
+++ lab07-group12/lab07-exercise01-3d/renderer.js
@@ -0,0 +1,76 @@
+"use strict";
+
+class Renderer {
+
+    /**
+    * Creates a new instance of Renderer. The given source code will be compiled
+    * and assembled into a WebGL ShaderProgram used by this shader to draw models.
+    * @param {string} shaderName the source code (text) of this shader programs shader.
+    */
+	constructor(shaderName){
+		this.program = GLUtils.createShaderProgram(shaderName);
+	}
+
+    /**
+    * Draws a model using this Renderers ShaderProgram.
+    * @param {ModelTransform} model the model to draw.
+    * @param {Object} shaderData whatever other data the ShaderProgram needs for drawing.
+    */
+    drawModel(model, camera, shaderData){
+
+        // activate this shader program
+        gl.useProgram(this.program);
+
+        // set the arrtibute arrays and uniform data for this programs vertex and
+        // fragment shader based on the models buffer data and material
+        this.setVertexAttributeArrays(model);
+        this.setUniformData(model, camera, shaderData);
+
+        // draw call using index based triangle assembly (elements)
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, model.mesh.bufIndex);
+        gl.drawElements(model.mesh.drawMode, model.mesh.indexCount, gl.UNSIGNED_SHORT, 0);
+
+        return this;
+	}
+
+    /**
+    * Sets ALL attributes for the vertex shader of this renderers shader program before drawing.
+    * @param {ModelTransform} model the model to draw.
+    */
+    setVertexAttributeArrays(model){
+        // position buffer
+        gl.bindBuffer(gl.ARRAY_BUFFER, model.mesh.positionBuffer);
+        let posAttribLoc = gl.getAttribLocation(this.program, "a_position");
+        gl.enableVertexAttribArray(posAttribLoc);
+        gl.vertexAttribPointer(posAttribLoc,3,gl.FLOAT,false,0,0);
+    }
+
+    /**
+    * Sets ALL uniforms for the vertex and fragment shader of this renderers shader program before drawing.
+    * @param {ModelTransform} model the model to draw, this has the model matrix and material attached.
+    * @param {Object} camera the camera used for rendering, or rather its view and projection matrix.
+    * @param {Object} shaderData whatever other data the Shader needs for drawing.
+    */
+    setUniformData(model, camera, shaderData){
+
+        // TODO: Set matrix uniform values. Remember you are now dealing with mat4!
+        //          You have to use the correct respective gl.uniformMatrix...() function for that datatype.
+        // TODO: Set model matrix uniform (u_matrixM) data.
+        let matrixLocation = gl.getUniformLocation(this.program, "u_matrixM");
+        gl.uniformMatrix4fv(matrixLocation, false, new Float32Array(model.modelMatrix));
+
+        // TODO: Set view matrix uniform (u_matrixV) data.
+        let viewLocation = gl.getUniformLocation(this.program, "u_matrixV");
+        gl.uniformMatrix4fv(viewLocation, false, new Float32Array(camera.viewMatrix));
+
+        // TODO: Set projection matrix uniform (u_matrixP) data.
+        let projLocation = gl.getUniformLocation(this.program, "u_matrixP");
+        gl.uniformMatrix4fv(projLocation, false, new Float32Array(camera.projectionMatrix));
+
+
+
+        // set tint color data
+        let colorLoc = gl.getUniformLocation(this.program, "u_tint");
+        gl.uniform3fv(colorLoc, model.material.tint.toFloat32());
+    }
+}
